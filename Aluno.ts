13e4fc44dab0a8f042ab66022b1fec3cdba46b47
fificici <114841536+fificici@users.noreply.github.com>
2024-08-26 19:59:21 -0300

    Aluno.ts

--- Aluno.ts
+++ Aluno.ts
@@ -27,10 +27,10 @@ export class AlunoII implements Aluno {
     }
 
     public getAluno(): void {
-        console.log(`Nota: ${this.nome}, Matricula: ${this.matricula}, P1: ${this.notaProva1}, P2: ${this.notaProva2}, Trabalho: ${this.notaTrabalho}`)
+        console.log(`Nome: ${this.nome}, Matricula: ${this.matricula}, P1: ${this.notaProva1}, P2: ${this.notaProva2}, Trabalho: ${this.notaTrabalho}`)
     }
 
-    public setDia(): void {
+    public setAluno(): void {
         let nomeUp = readline.question('Nome: ')
         let matriculaUp = readline.questionInt('Matricula: ')
         let notaProva1Up = readline.questionInt('P1: ')
@@ -56,5 +56,59 @@ export class AlunoII implements Aluno {
 }
 
 let novoAluno = new AlunoII (1, 'Jão', 10, 5, 2)
-novoAluno.media()
-novoAluno.final()
+
+function menu() {
+    console.log(`===== escolha aí =====\n 1. getAluno\n 2. setAluno\n 3. media\n 4. final\n 5. sair`)
+
+}
+
+function escolha(opcao: string) {
+    switch (opcao) {
+
+        case '1':
+
+        novoAluno.getAluno()
+
+            break;
+
+        case '2':
+
+        novoAluno.setAluno()
+
+            break;
+
+        case '3':
+
+        console.log(novoAluno.media())
+
+            break;
+
+        case '4':
+
+        console.log(novoAluno.final())
+
+            break;
+
+        case '5':
+
+            console.log("saindo...");
+           
+            return true; 
+
+        default:
+            console.log("Opção inválida. Por favor, escolha uma opção válida.");
+    }
+    return false; 
+}
+
+function main() {
+    let encerrar = false;
+
+    while (!encerrar) {
+        menu();
+        let opcao = readline.question("Escolha uma opcao: ");
+        encerrar = escolha(opcao);
+    }
+}
+
+main()
