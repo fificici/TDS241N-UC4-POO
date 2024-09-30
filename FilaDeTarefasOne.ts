304188ee647ad693b370cd5ae80426ffba5f5e80
fificici <114841536+fificici@users.noreply.github.com>
2024-09-30 20:14:53 -0300

    Update FilaDeTarefasOne.ts

--- FilaDeTarefasOne.ts
+++ FilaDeTarefasOne.ts
@@ -3,7 +3,8 @@ interface inTarefa {
     status: string
 
     getTarefa(): void
-}
+} 
+//interface da classe Tarefa
 
 export class Tarefa implements inTarefa {
     public descricao: string
@@ -17,6 +18,7 @@ export class Tarefa implements inTarefa {
     public getTarefa(): void {
         console.log(`Descrição: ${this.descricao}\nStatus: ${this.status}`)
     }
+    //get que mostra os atributos da classe
 }
 
 interface inFilaDeTarefas {
@@ -25,7 +27,9 @@ interface inFilaDeTarefas {
     remover_tarefa(): Tarefa | undefined
     listar_tarefas(): void
     tarefa_pronta(): void
+    concluirTarefa(index: number): void
 }
+//interface da classe FilDeTarefas
 
 export class FilaDeTarefas implements inFilaDeTarefas {
     public fila: Tarefa[] = []
@@ -33,18 +37,33 @@ export class FilaDeTarefas implements inFilaDeTarefas {
     public adicionar_tarefa(tarefa: Tarefa): void {
         this.fila.push(tarefa) 
     }
+    //enfileira a Tarefa no vetor
 
     public remover_tarefa(): Tarefa | undefined {
         return this.fila.shift()
     }
+    //remove a primeira Tarefa no vetor
 
     public listar_tarefas(): void {
         console.log(this.fila)
     }
+    //printa todas as Tarefas
 
     public tarefa_pronta(): void {
-        this.fila[0].status = 'concluído'
+        for (let i = 0; i < this.fila.length; i++) {
+            if (this.fila[i].status == 'pendente') {
+                this.concluirTarefa(i)
+                console.log(this.fila[i])
+                break
+            } 
+        }
     }
+    //procura pelo indice o primeiro status que for 'pendente', entao chama o concluirTarefa() e coloca o indice como parametro para efetuar a troca para 'concluido'
+
+    public concluirTarefa(index: number): void{
+        this.fila[index].status = 'concluido'
+    }
+    //muda o atributo status da Tarefa para 'concluido' de acordo com o index
 }
 
 let readlineSync = require('readline-sync')
@@ -52,7 +71,7 @@ let readlineSync = require('readline-sync')
 let filaNova = new FilaDeTarefas()
 
 function menu() {
-    console.log(`===== Fila de Tarefas =====\n 1. adicionar\n 2. remover\n 3. listar\n 4. marcar como concluídas\n 5. sair`)
+    console.log(`\n===== Fila de Tarefas =====\n 1. adicionar\n 2. remover\n 3. listar\n 4. marcar como concluídas\n 5. sair`)
 }
 
 
@@ -60,18 +79,19 @@ function escolha(opcao) {
     switch (opcao) {
 
         case '1':
+            
+            console.log("\nadicionar\n");
 
-            console.log("adicionar");
             let perguntaTarefa = readlineSync.question('Insira descricao da tarefa: ')
-            this.Tarefa.descricao = perguntaTarefa
-            filaNova.adicionar_tarefa(perguntaTarefa)
-            console.log(`Tarefa Inserida: ${perguntaTarefa}`)
+            let novaTarefa = new Tarefa(perguntaTarefa, 'pendente')
+            filaNova.adicionar_tarefa(novaTarefa)
+            console.log(`Tarefa Inserida: ${perguntaTarefa}\nStatus: pendente`)
 
             break;
 
         case '2':
 
-            console.log("remover")
+            console.log("\nremover\n")
 
             console.log(filaNova.remover_tarefa())
 
@@ -79,7 +99,7 @@ function escolha(opcao) {
 
         case '3':
 
-            console.log("listar");
+            console.log("\nlistar\n");
 
             filaNova.listar_tarefas()
 
@@ -87,9 +107,9 @@ function escolha(opcao) {
         
         case '4':
 
-            console.log('marcar como conclúida')
+            console.log('\nmarcar como conclúida\n')
 
-            console.log(filaNova.tarefa_pronta())
+            filaNova.tarefa_pronta()
 
             break;
 
@@ -110,7 +130,7 @@ function main() {
 
     while (!encerrar) {
         menu();
-        let opcao = readlineSync.question("Escolha uma opcao: ");
+        let opcao = readlineSync.question("\nEscolha uma opcao: ");
         encerrar = escolha(opcao);
     }
 }
